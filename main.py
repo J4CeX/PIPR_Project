6c5c39ac5db05b923310958c4f2079259be20d85
Jacek <jacek_kozakowski_dev@wp.pl>
2024-12-25 18:11:18 +0100

    Create menu file

--- main.py
+++ main.py
@@ -1,67 +1,22 @@
-from space import Space
-from objects import (
-    OrbitalObject,
-    CentralObject
+from menu import (
+    menu_print,
+    new_data,
+    load_data
 )
 import os
 
 
-def menu_print(width):
-    print('*' * width)
-    print('SIMULATION OF MOTION IN A GRAVITATIONAL FIELD'.center(width))
-    print('*' * width)
-    print('Options:')
-    print('1. New simulation')
-    print('2. Load simulation')
-    print('0. Exit')
-
-
-def new_data():
-    print('Field name:')
-    field_name = str(input('>> '))
-    print('Field size:')
-    size = int(input('>> '))
-    print('Central object')
-    print('Diameter:')
-    CO_diameter = int(input('>> '))
-    print('Mass:')
-    CO_mass = int(input('>> '))
-    orbital_objects = []
-    print('Orbital objects')
-    print('Number of objects:')
-    quantity = int(input('>> '))
-    for index in range(0, quantity):
-        print(f'Object: {index+1}.')
-        print('Mass:')
-        OO_mass = int(input('>> '))
-        print('Position:')
-        OO_x = int(input('(x) >> '))
-        OO_y = int(input('(y) >> '))
-        OO_position = (OO_x, OO_y)
-        print('Velocity:')
-        OO_velocity = int(input('>> '))
-        orbital_object = OrbitalObject(OO_mass, OO_position, OO_velocity)
-        orbital_objects.append(orbital_object)
-    os.system('cls')
-    central_object = CentralObject(CO_mass, CO_diameter)
-    return Space(size, central_object, orbital_objects, field_name)
-
-
-def load_data():
-    pass
-
-
 def main():
-    width = 40
     while True:
         os.system('cls')
-        menu_print(width)
+        menu_print()
         option = input('>> ')
         if option == '1':
             space = new_data()
             print('Enter steps number: ')
             steps = int(input('>> '))
             space.simulate(steps)
+            # wyniki oraz możliwość ich zapisu do pliku
         elif option == '2':
             # wczytywanie wyników poprzedniej symulacji
             space = load_data()
---
+++ menu.py
@@ -0,0 +1,66 @@
+from space import Space
+from objects import (
+    OrbitalObject,
+    CentralObject
+)
+import os
+
+
+def header():
+    width = 40
+    print('*' * width)
+    print('SIMULATION OF MOTION IN A GRAVITATIONAL FIELD'.center(width))
+    print('*' * width)
+
+
+def clean():
+    os.system('cls')
+    header()
+
+
+def menu_print():
+    clean()
+    print('Options:')
+    print('1. New simulation')
+    print('2. Load simulation')
+    print('0. Exit')
+
+
+def new_data():
+    clean()
+    print('Field name:')
+    field_name = str(input('>> '))
+    print('Field size:')
+    size = int(input('>> '))
+    clean()
+    print('Central object')
+    print('Diameter:')
+    CO_diameter = int(input('>> '))
+    print('Mass:')
+    CO_mass = int(input('>> '))
+    orbital_objects = []
+    clean()
+    print('Orbital objects')
+    print('Number of objects:')
+    quantity = int(input('>> '))
+    for index in range(0, quantity):
+        clean()
+        print('Orbital objects')
+        print(f'Object: {index+1}.')
+        print('Mass:')
+        OO_mass = int(input('>> '))
+        print('Position:')
+        OO_x = int(input('(x) >> '))
+        OO_y = int(input('(y) >> '))
+        OO_position = (OO_x, OO_y)
+        print('Velocity:')
+        OO_velocity = int(input('>> '))
+        orbital_object = OrbitalObject(OO_mass, OO_position, OO_velocity)
+        orbital_objects.append(orbital_object)
+    clean()
+    central_object = CentralObject(CO_mass, CO_diameter)
+    return Space(size, central_object, orbital_objects, field_name)
+
+
+def load_data():
+    pass
