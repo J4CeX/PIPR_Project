37e4c1b5b87936814bbad9769f3c8166b747c74f
Jacek <jacek_kozakowski_dev@wp.pl>
2024-12-25 16:03:26 +0100

    Removed Simulation and moved its methods to Space

--- space.py
+++ space.py
@@ -6,34 +6,39 @@ from objects import (
     OrbitalObject,
     CentralObject
 )
+from PIL import (
+    Image,
+    ImageDraw
+)
 
 
 class Space:
     def __init__(self, size: int, central_object: CentralObject,
                  orbital_objects: OrbitalObject = [], space_name='unknown'):
+        space_image = Image.new('RGB', (size, size), (0, 0, 0))
+        draw = ImageDraw.Draw(space_image)
+        radius = central_object.diameter() / 2
+        position = (size / 2, size / 2)
+        draw.circle(position, radius, fill='white')
+        for object in orbital_objects:
+            draw.point(object.position)
         self._space_name = space_name
         self._size = size
         self.central_object = central_object
         self.orbital_objects = orbital_objects
+        self.space_image = space_image
 
-    def print(self):
-        pass
-        # path = f'simulations/{self.space_name()}.png'
-        # space_image = Image.new('RGB', (self.size(), self.size()), (0, 0, 0))
-        # draw = ImageDraw.Draw(space_image)
-        # radius = self.central_object.diameter() / 2
-        # position = (self.size() / 2, self.size() / 2)
-        # draw.circle(position, radius, fill='white')
-        # for object in self.orbital_objects:
-        #     draw.point(object.position)
-        # space_image.save(path)
-        # space_image.show()
+    def simulate(self, steps: int):
+        # for step in range(0, steps):
+        #     for object in self
+        self.space_image.show()
 
     def open(self):
         pass
 
     def save(self):
-        pass
+        path = f'simulations/{self.space_name()}.png'
+        self.space_image.save(path)
 
     def size(self):
         return self._size
--- tests/test_space.py
+++ tests/test_space.py
@@ -14,3 +14,23 @@ def test_Space_create():
     assert space.central_object.mass() == 2000
     assert space.orbital_objects[0].mass() == 200
     assert space.orbital_objects[1].mass() == 100
+
+
+def test_Space_simulate():
+    central_object = CentralObject(2000, 100)
+    orbital_objects = [
+        OrbitalObject(200, (2, 2), 2),
+        OrbitalObject(100, (10, 5), 2)
+    ]
+    space = Space(500, central_object, orbital_objects, 'test')
+    space.simulate(1)
+
+
+def test_Space_save():
+    central_object = CentralObject(2000, 100)
+    orbital_objects = [
+        OrbitalObject(200, (2, 2), 2),
+        OrbitalObject(100, (10, 5), 2)
+    ]
+    space = Space(500, central_object, orbital_objects, 'test')
+    space.save()
